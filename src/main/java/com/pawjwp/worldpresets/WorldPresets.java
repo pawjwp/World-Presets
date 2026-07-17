86936bf4e49721c375c481bc16d101e81d952165
Jeremy <pawjwp@users.noreply.github.com>
2026-07-16 22:02:10 -0400

    Renamed mod to just "World Presets"

--- src/main/java/com/pawjwp/worldpresets/WorldPresets.java
+++ src/main/java/com/pawjwp/worldpresets/WorldPresets.java
@@ -1,19 +1,19 @@
-package com.pawjwp.worldcreation;
+package com.pawjwp.worldpresets;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(WorldCreation.MODID)
-public class WorldCreation
+@Mod(WorldPresets.MODID)
+public class WorldPresets
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "worldcreation";
+    public static final String MODID = "worldpresets";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public WorldCreation()
+    public WorldPresets()
     {
         // Most behavior will be in @Mod.EventBusSubscriber classes and mixins
     }
